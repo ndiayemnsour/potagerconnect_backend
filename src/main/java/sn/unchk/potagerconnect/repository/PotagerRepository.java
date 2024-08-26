788935fd0ca1f9714f852a5e26f67a979e7bf7ef
ndiayemnsour <zocod3r@gmail.com>
2024-08-26 15:23:29 +0000

    ajpout du fichier sql  de la base de donnee

--- src/main/java/sn/unchk/potagerconnect/repository/PotagerRepository.java
+++ src/main/java/sn/unchk/potagerconnect/repository/PotagerRepository.java
@@ -4,6 +4,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import sn.unchk.potagerconnect.model.Potager;
 
 
-public interface PotagerRepository extends JpaRepository<Potager, String> {
+public interface PotagerRepository extends JpaRepository<Potager, Integer> {
 }
 
